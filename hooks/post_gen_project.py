4e1d917719ca566c2b55140b96965470e6323b5c
Kapil Agrawal <7047165+netops2devops@users.noreply.github.com>
2026-01-15 19:22:44 -0600

    When a user is promted for create_git_repo and if the user answers with a no, there is no point asking about which CI platform they would like to use

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,8 +14,17 @@ def init_git():
         print("Git repository initialized with initial commit.")
 
 def remove_unused_ci_files():
-    """Remove CI files that aren't needed based on selected platform."""
+    """Remove CI files that aren't needed based on selected platform.
+
+    If create_git_repo is 'no', CI platform is forced to 'none' since
+    CI/CD pipelines require a git repository.
+    """
+    create_git_repo = "{{ cookiecutter.create_git_repo }}"
     ci_platform = "{{ cookiecutter.ci_platform }}"
+
+    # CI makes no sense without a git repo - force to 'none'
+    if create_git_repo != "yes":
+        ci_platform = "none"
 
     if ci_platform != "github":
         github_dir = ".github"
